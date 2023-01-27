5b7249db00310c5c0b1c339c6d1a49ba3646ce67
jvillad <jyvillad@gmail.com>
2023-01-27 18:55:01 +1100

    separate file for fonts inside lib folder

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,29 +1,17 @@
-import './globals.css';
-import { Ubuntu, Roboto, Fira_Sans } from '@next/font/google';
+import Nav from '@/components/Nav';
 
-const ubuntu = Ubuntu({
-   subsets: ['latin'],
-   weight: ['400', '700'],
-   variable: '--font-ubuntu',
-});
-
-const roboto = Roboto({
-   subsets: ['latin'],
-   weight: ['400', '700'],
-   variable: '--font-roboto',
-});
-
-const firaSans = Fira_Sans({
-   subsets: ['latin'],
-   weight: ['400', '700'],
-   variable: '--font-firaSans',
-});
+import { firaSans } from '@/lib/fonts.js';
 
 export default function RootLayout({ children }) {
    return (
       <html lang="en">
          <head />
-         <body className={`${firaSans.className} mx-24 py-14`}>{children}</body>
+         <body className={`${firaSans.className}`}>
+            <>
+               <Nav />
+            </>
+            <div className="mx-24 py-14">{children}</div>
+         </body>
       </html>
    );
 }
