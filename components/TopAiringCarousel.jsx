bfa142075f64b1e0adf7b333ebd6681a43077b8f
jvillad <jyvillad@gmail.com>
2023-01-28 20:58:08 +1100

    top airing image carousel

---
+++ components/TopAiringCarousel.jsx
@@ -0,0 +1,39 @@
+'use client';
+import Image from 'next/image';
+import Link from 'next/link';
+import Slider from 'react-slick';
+import { sliderSettings } from '@/lib/sliderSettings';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+export default function ImageCarousel({ data }) {
+   return (
+      <div className="mt-24">
+         <h1 className="p-2 font-bold">Top Airing Anime</h1>
+         <Slider {...sliderSettings}>
+            {data?.map((image) => {
+               return (
+                  <div key={image.id}>
+                     <div className="w-[170px]">
+                        <Link href={`${image.id}`}>
+                           <Image
+                              src={image.url}
+                              alt={`an image of ${image.title}`}
+                              width={170}
+                              height={240}
+                              style={{
+                                 width: 170,
+                                 height: 240,
+                              }}
+                              className="rounded-xl"
+                           />
+                        </Link>
+                        <div className="text-xs w-[170px]">{image.title}</div>
+                     </div>
+                  </div>
+               );
+            })}
+         </Slider>
+      </div>
+   );
+}
