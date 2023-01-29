de37ef1c3b4ce323b3842d26f269001d70041ba8
jvillad <jyvillad@gmail.com>
2023-01-29 21:36:26 +1100

    sort api by members instead of generic top anime

--- app/ranking/page.jsx
+++ app/ranking/page.jsx
@@ -1,7 +1,10 @@
 import TopAnime from '@/components/TopAnime';
 
 export default async function AnimeRank() {
-   const data = await fetch(`https://api.jikan.moe/v4/top/anime`);
+   // const data = await fetch(`https://api.jikan.moe/v4/top/anime`);
+   const data = await fetch(
+      `https://api.jikan.moe/v4/anime?&order_by=members&sort=desc`
+   );
    const response = await data.json();
    return (
       <div className="grid gap-10 grid-cols-fluid">
