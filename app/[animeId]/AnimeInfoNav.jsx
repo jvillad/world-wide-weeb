d6ebae0b7aef6b3ce0ade92f8df12b0162cabf04
jvillad <jyvillad@gmail.com>
2023-02-01 23:54:45 +1100

    responsive sub navbar for anime details page

--- app/[animeId]/AnimeInfoNav.jsx
+++ app/[animeId]/AnimeInfoNav.jsx
@@ -1,17 +1,45 @@
-import React from 'react';
+'use client';
+import { useState } from 'react';
 
 export default function AnimeInfoNav() {
+   const [open, setOpen] = useState(false);
    return (
-      <nav className="pb-2">
-         {/* Anime details nav main conntainer */}
-         <div className=" mx-auto">
-            {/* wrapper for all anime details links */}
-            <div className="">
-               <div className="flex flex-row-reverse items-center space-x-6 space-x-reverse">
-                  <a href="#">Characters</a>
-                  <a href="#">Episodes</a>
-                  <a href="#">Reviews</a>
-                  <a href="#">News</a>
+      <nav className=" flex justify-center items-center">
+         <div className="rounded-md w-full bg-gray-800 xs:min-w-[250px]">
+            <div className="flex flex-row-reverse p-2 md:hidden">
+               <button
+                  onClick={() => {
+                     setOpen(!open);
+                  }}
+               >
+                  <svg
+                     xmlns="http://www.w3.org/2000/svg"
+                     fill="none"
+                     viewBox="0 0 24 24"
+                     strokeWidth={1.5}
+                     stroke="currentColor"
+                     className="w-6 h-6 text-teal-500"
+                  >
+                     <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z"
+                     />
+                  </svg>
+               </button>
+            </div>
+            {/* Anime details nav main container */}
+            <div className="mx-auto">
+               {/* wrapper for all anime details links */}
+               <div
+                  className={`w-full md:block md:w-auto ${!open && 'hidden'}`}
+               >
+                  <div className="flex flex-col-reverse md:flex md:flex-row-reverse items-center md:space-x-8 md:space-x-reverse text-teal-500 p-2 mr-3">
+                     <a href="#">Characters</a>
+                     <a href="#">Episodes</a>
+                     <a href="#">Reviews</a>
+                     <a href="#">News</a>
+                  </div>
                </div>
             </div>
          </div>
--- app/[animeId]/layout.jsx
+++ app/[animeId]/layout.jsx
@@ -1,9 +1,9 @@
 import AnimeInfoNav from './AnimeInfoNav';
 export default function Layout({ children }) {
    return (
-      <>
+      <div className="mx-20">
          <AnimeInfoNav />
          {children}
-      </>
+      </div>
    );
 }
