8451984a72e1df4a3ad982a346bb7d2c9aca139e
jvillad <jyvillad@gmail.com>
2023-02-01 23:53:26 +1100

    custom min-width 320px

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,20 @@ module.exports = {
             fluid: 'repeat(auto-fit,minmax(15rem,1fr))',
          },
       },
+      screens: {
+         xs: '320px',
+         // => @media (min-width: 320px) { ... }
+         sm: '645px',
+         // => @media (min-width: 640px) { ... }
+         md: '768px',
+         // => @media (min-width: 768px) { ... }
+         lg: '1024px',
+         // => @media (min-width: 1024px) { ... }
+         xl: '1280px',
+         // => @media (min-width: 1280px) { ... }
+         '2xl': '1536px',
+         // => @media (min-width: 1536px) { ... }
+      },
    },
    plugins: [],
 };
