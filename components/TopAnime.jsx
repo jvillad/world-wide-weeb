a73d705297c7f148e458664166147c3a293ad69e
jvillad <jyvillad@gmail.com>
2023-02-05 23:38:51 +1100

    add background color and set title text to white

--- components/TopAnime.jsx
+++ components/TopAnime.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export default function TopAnime({ title, id, img }) {
    return (
-      <div className="grid place-items-center p-2 rounded">
+      <div className="grid place-items-center p-2 rounded-xl bg-gray-900">
          <Link href={`${id}`}>
             <Image
                src={img.image_url}
@@ -17,7 +17,7 @@ export default function TopAnime({ title, id, img }) {
                className="rounded-xl"
             />
             <div className="w-[230px]">
-               <h1 className="text-sm p-2">{title}</h1>
+               <h1 className="text-sm p-2 text-white">{title}</h1>
             </div>
          </Link>
       </div>
