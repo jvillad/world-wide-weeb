6b96bc1a0f4bb64d33f0e34417b563458cd625ad
jvillad <jyvillad@gmail.com>
2023-02-06 21:27:37 +1100

    added prev and next button to the image carousel

--- components/ImageCarousel.jsx
+++ components/ImageCarousel.jsx
@@ -3,13 +3,18 @@ import Image from 'next/image';
 import Link from 'next/link';
 import Slider from 'react-slick';
 import { sliderSettings } from '@/lib/sliderSettings';
+import { useRef } from 'react';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 export default function ImageCarousel({ data }) {
+   const slider = useRef(null);
+   const nextSlide = () => slider.current.slickNext();
+   const prevSlide = () => slider.current.slickPrev();
+
    return (
-      <>
-         <Slider {...sliderSettings}>
+      <div className="relative">
+         <Slider {...sliderSettings} ref={slider} className="mx-10">
             {data?.map((image) => {
                return (
                   <div key={image.id}>
@@ -33,6 +38,41 @@ export default function ImageCarousel({ data }) {
                );
             })}
          </Slider>
-      </>
+
+         <div className="absolute top-0 mt-[100px] flex justify-between w-full">
+            <button onClick={prevSlide}>
+               <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-8 h-8 stroke-2 text-gray-400"
+               >
+                  <path
+                     strokeLinecap="round"
+                     strokeLinejoin="round"
+                     d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"
+                  />
+               </svg>
+            </button>
+            <button onClick={nextSlide}>
+               <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-8 h-8 stroke-2 text-gray-400"
+               >
+                  <path
+                     strokeLinecap="round"
+                     strokeLinejoin="round"
+                     d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5"
+                  />
+               </svg>
+            </button>
+         </div>
+      </div>
    );
 }
