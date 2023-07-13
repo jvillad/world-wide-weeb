05957424da00d3adf1428e1d5d60fdfe31cc80fd
johnrey.villademosa <jyvillad@gmail.com>
2023-07-13 22:16:08 +1000

    link head and add favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,18 +1,19 @@
 import Nav from '@/components/Nav';
-
+import Head from './head';
 import { roboto } from '@/lib/fonts.js';
 
 export default function RootLayout({ children }) {
    return (
       <html lang="en">
-         <head />
+         <Head />
+
          <body className={`${roboto.className}`}>
             <>
                <Nav />
             </>
             <div>{children}</div>
             <div className="text-xs text-center my-10 mx-10 text-gray-600">
-               AnimeDB - 🫥{' '}
+               World Wide Weeb
             </div>
          </body>
       </html>
