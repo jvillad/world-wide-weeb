0f9c18921c20a2c57b4c80efb149f45acd16cd52
johnrey.villademosa <jyvillad@gmail.com>
2023-07-15 22:51:03 +1000

    Wrap app body to session provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,21 +1,23 @@
 import Nav from '@/components/Nav';
 import Head from './head';
 import { roboto } from '@/lib/fonts.js';
+import Provider from '@/app/Provider';
 
 export default function RootLayout({ children }) {
    return (
       <html lang="en">
          <Head />
-
-         <body className={`${roboto.className}`}>
-            <>
-               <Nav />
-            </>
-            <div>{children}</div>
-            <div className="text-xs text-center my-10 mx-10 text-gray-600">
-               World Wide Weeb
-            </div>
-         </body>
+         <Provider>
+            <body className={`${roboto.className}`}>
+               <>
+                  <Nav />
+               </>
+               <div>{children}</div>
+               <div className="text-xs text-center my-10 mx-10 text-gray-600">
+                  World Wide Weeb
+               </div>
+            </body>
+         </Provider>
       </html>
    );
 }
